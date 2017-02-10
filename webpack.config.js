3842d1b30d08e3f06f420752025af5d8ca86292a
puku0x <puku0x@gmail.com>
2017-02-10 18:24:06 +0900

    webpack2

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) => ({
+  entry: {
+    app: './src/js/app.js',
+    vendor: ['angular', 'onsenui'],
+  },
+  output: {
+    path: path.join(__dirname, '/www/assets'),
+    filename: '[name].bundle.js',
+  },
+  devtool: '#inline-source-map',
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
+    }),
+    new ExtractTextPlugin({
+      filename: "[name].bundle.css",
+      allChunks: true
+    })
+  ],
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },{
+        test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+        use: 'file-loader'
+      },{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['ng-annotate-loader', 'babel-loader']
+      }
+    ]
+  },
+  devServer: {
+    inline: true,
+  },
+});
